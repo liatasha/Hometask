50e6ff7d024928d207229cb7159b342ebb2da1fc
Peculiar <oladehindepeculiar@gmail.com>
2026-02-08 17:50:29 +0100

    first commit

---
+++ readme.txt
@@ -0,0 +1,4 @@
+#Hometask
+
+Practice all you have learnt on git/github, then, push to you github account.
+Push the Python notebook to your github account.
---
+++ string.py
@@ -0,0 +1,9 @@
+#Creating a login system for student portal.
+
+password = input("Please enter your password: ")
+email = input("Please enter your email account: ")
+
+if password == "123456" and email == "oladehindepeculiar@gmail.com":
+  print("Hello Peculiar, you have successfully login in! ")
+else:
+  print("Incorrect password, try again.")
